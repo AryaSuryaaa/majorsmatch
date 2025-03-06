c32dd21797da107a50365dcdf7be7a1d2b8aa44f
AryaSurya <suryasaputra914@gmail.com>
2025-03-06 10:08:12 +0700

    clean profile

--- app/src/main/java/com/anugrah/majorsmatch/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/anugrah/majorsmatch/ui/screen/profile/ProfileScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
@@ -29,11 +30,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.anugrah.majorsmatch.navigation.screen.Screen
 import com.anugrah.majorsmatch.ui.components.DynamicDialog
+import com.anugrah.majorsmatch.ui.theme.MajorsmatchTheme
 import com.anugrah.majorsmatch.utils.showToast
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -45,17 +49,24 @@ fun ProfileScreen(
   Column(
     modifier = Modifier.fillMaxSize()
   ) {
-    AccountContent(navController = navHostController)
+    AccountContent(
+      onLogout = {
+        navHostController.navigate(Screen.Login.route) {
+          popUpTo(navHostController.graph.id) {
+            inclusive = true
+          }
+        }
+      }
+    )
   }
 }
 
 @Composable
 fun AccountContent(
-  navController: NavHostController,
+  onLogout: () -> Unit
 ) {
 
   val (showLogOutDialog, setShowLogOutDialog) = remember { mutableStateOf(false) }
-  var actionOnConfirmWarning by remember { mutableStateOf({}) }
   val context = LocalContext.current
 
   // Dialog untuk Log out
@@ -65,13 +76,7 @@ fun AccountContent(
     title = "Log Out",
     message = "Are you sure you want to log out?",
     onConfirm = {
-//      sessionViewModel.logout()
-      navController.navigate(Screen.Login.route) {
-        popUpTo(navController.graph.id) {
-          inclusive = true
-        }
-      }
-
+      onLogout()
     },
     onDismissButton = { /* Optional */ }
   )
@@ -80,7 +85,7 @@ fun AccountContent(
     modifier = Modifier
       .fillMaxSize()
       .verticalScroll(rememberScrollState())
-      .padding(16.dp)
+      .padding(16.dp),
   ) {
     Column(
       modifier = Modifier
@@ -98,95 +103,31 @@ fun AccountContent(
       )
     }
 
-    Divider(modifier = Modifier.padding(vertical = 8.dp))
+    HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
 
     // Bagian "Account Settings"
-    Text(
-      text = "Account Settings",
-      style = MaterialTheme.typography.bodyMedium,
-      color = Color.Gray,
-      modifier = Modifier.padding(vertical = 8.dp)
-    )
+    SectionTitle(title = "Account Settings")
 
     AccountItem(
       title = "Language",
-      onClick = {
-        context.showToast("This feature is not available now")
-      }
+      onClick = { context.showToast("This feature is not available now") }
     )
 
-    // Push notifications switch
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .padding(vertical = 4.dp),
-      verticalAlignment = Alignment.CenterVertically,
-      horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-      Text(text = "Push notifications", style = MaterialTheme.typography.bodyLarge)
-      Switch(
-        checked = false,
-        onCheckedChange = { /* Toggle Push Notifications */ },
-      )
-    }
+    ToggleItem(title = "Push notifications", checked = false, onCheckedChange = {})
+    ToggleItem(title = "Dark mode", checked = false, onCheckedChange = {})
 
-//     Dark mode switch
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .padding(vertical = 4.dp),
-      verticalAlignment = Alignment.CenterVertically,
-      horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-      Text(text = "Dark mode", style = MaterialTheme.typography.bodyLarge)
-      Switch(
-        checked = false,
-        onCheckedChange = { /* Toggle Dark Mode */ }
-      )
-    }
-
-    Divider(modifier = Modifier.padding(vertical = 8.dp))
+    HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
 
     // Bagian "More"
-    Text(
-      text = "More",
-      style = MaterialTheme.typography.bodyMedium,
-      color = Color.Gray,
-      modifier = Modifier.padding(vertical = 8.dp)
-    )
+    SectionTitle(title = "More")
 
-    AccountItem(
-      title = "About App",
-      onClick = {
-        context.showToast("This feature is not available now")
-      }
-    )
-    AccountItem(
-      title = "Privacy policy",
-      onClick = {
-        context.showToast("This feature is not available now")
-      }
-    )
-    AccountItem(
-      title = "Terms and conditions",
-      onClick = {
-        context.showToast("This feature is not available now")
-      }
-    )
+    AccountItem(title = "About App", onClick = { context.showToast("This feature is not available now") })
+    AccountItem(title = "Privacy policy", onClick = { context.showToast("This feature is not available now") })
+    AccountItem(title = "Terms and conditions", onClick = { context.showToast("This feature is not available now") })
 
     Spacer(modifier = Modifier.height(16.dp))
-    // Log out option
-    Button(
-      onClick = {
-        setShowLogOutDialog(true)
-      },
-      colors = ButtonDefaults.buttonColors(
-        containerColor = MaterialTheme.colorScheme.error,
-      ),
-      modifier = Modifier.fillMaxWidth()
-    ) {
-      Text(text = "Log out")
-    }
+
+    LogoutButton { setShowLogOutDialog(true) }
   }
 }
 
@@ -208,5 +149,48 @@ fun AccountItem(
       style = MaterialTheme.typography.bodyLarge,
       color = MaterialTheme.colorScheme.onBackground
     )
+  }
+}
+
+@Composable
+fun SectionTitle(title: String) {
+  Text(
+    text = title,
+    style = MaterialTheme.typography.bodyMedium,
+    color = Color.Gray,
+    modifier = Modifier.padding(vertical = 8.dp)
+  )
+}
+
+@Composable
+fun ToggleItem(title: String, checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(vertical = 4.dp),
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.SpaceBetween
+  ) {
+    Text(text = title, style = MaterialTheme.typography.bodyLarge)
+    Switch(checked = checked, onCheckedChange = onCheckedChange)
+  }
+}
+
+@Composable
+fun LogoutButton(onClick: () -> Unit) {
+  Button(
+    onClick = onClick,
+    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
+    modifier = Modifier.fillMaxWidth()
+  ) {
+    Text(text = "Log out")
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProfileScreenPreview() {
+  MajorsmatchTheme {
+    AccountContent{}
   }
 }
